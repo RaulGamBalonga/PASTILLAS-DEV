e58a4e5dc692558bffdf8542dd958193799779dd
gambalongadev <gambalongadev@gmail.com>
2024-02-03 22:31:31 +0100

    Add CSS styles for truncating text and display articles in a grid

---
+++ Trucazo-archivos-con-touch/trucazo-aqui.js
@@ -0,0 +1,3 @@
+// Crear varios archivos de una sola vez con touch
+// touch {button,select}.js
+// $ touch ./src/components/{Hero,About,Experience,Education,Skills,Projects}.astro                                                            
