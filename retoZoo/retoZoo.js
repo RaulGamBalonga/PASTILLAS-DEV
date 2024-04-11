1f91d71aab2805f89cc72261cdfa1a8c34c6f8d4
gambalongadev <gambalongadev@gmail.com>
2024-04-11 22:34:35 +0200

    Fix variable name in retoZoo.js file

--- retoZoo/retoZoo.js
+++ retoZoo/retoZoo.js
@@ -3,3 +3,9 @@ const zoo2 = zoo.replaceAll('🐍', '🐘');
 console.log(zoo2); // 🐇🦖🐘🐪🐘
 const result = zoo2.includes('🐍');
 console.log(result); // true 
+
+const animals = '🐇🦖🐍🐪🐍';
+const home = animals.replace('🐍', '🐘');
+console.log(zoo2); // 🐇🦖🐘🐪🐘
+const result2 = zoo2.includes('🐍');
+console.log(result); // true 
