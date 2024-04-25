fda0958277a5a947560977a6a4773e3fe5165b05
gambalongadev <gambalongadev@gmail.com>
2024-04-25 21:57:24 +0200

    Add code for working with different scopes in reto-js7.js

---
+++ reto-js/reto-js7.js
@@ -0,0 +1,10 @@
+function printer(num) {
+    // Write your code below this line
+    if(num === 5) {
+        return num
+    } else {
+        return "The number is not 5"
+    }
+}
+console.log(printer(5))
+console.log(printer(3))
