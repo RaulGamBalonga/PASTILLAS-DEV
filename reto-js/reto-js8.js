b0461c09bdba72ff1c99955dfcd423f54db882ea
gambalongadev <gambalongadev@gmail.com>
2024-04-29 23:23:51 +0200

    Add code for incrementing and decrementing numbers in reto-js8.js

---
+++ reto-js/reto-js8.js
@@ -0,0 +1,8 @@
+// You are given two numbers num1 and num2. Increment the first number by 1. Decrement the second number by 1. Then, print the difference between the first number and the second.
+
+function math(num1, num2) {
+    // Write your code below this line
+    num1++;
+    num2--;
+    console.log(num1 - num2);
+}
